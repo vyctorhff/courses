45d3f3ff4813c4988345d95917bd2f2503f74061
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-03-05 21:52:38 -0300

    pos-java: socket

--- pos-java-elite/1 - Java Fundamentals/fundamentals/src/main/java/br/course/elita/java/server/domain/userCase/handler/PathHandlerEnum.java
+++ pos-java-elite/1 - Java Fundamentals/fundamentals/src/main/java/br/course/elita/java/server/domain/userCase/handler/PathHandlerEnum.java
@@ -1,13 +1,16 @@
 package br.course.elita.java.server.domain.userCase.handler;
 
 public enum PathHandlerEnum {
-    LIST("GET /socket/list", new ListHandler()),
-    CREATE("POST /socket/create", new CreateHandler());
+    LIST("GET", "/list", new ListHandler()),
+    CREATE("POST", "/create", new CreateHandler()),
+    UPDATE("PUT", "/update", new CreateHandler());
 
+    private final String httpVerbo;
     private final String path;
     private final SocketHttpHandler handler;
 
-    PathHandlerEnum(String path, SocketHttpHandler handler) {
+    PathHandlerEnum(String httpVerbo, String path, SocketHttpHandler handler) {
+        this.httpVerbo = httpVerbo;
         this.path = path;
         this.handler = handler;
     }
@@ -18,5 +21,9 @@ public enum PathHandlerEnum {
 
     public SocketHttpHandler getHandler() {
         return handler;
+    }
+
+    public boolean isRequestValid(String content) {
+        return content.contains("%s %s ".formatted(httpVerbo, path));
     }
 }
--- pos-java-elite/1 - Java Fundamentals/fundamentals/src/main/java/br/course/elita/java/server/socket/SingleThreadSocketServer.java
+++ pos-java-elite/1 - Java Fundamentals/fundamentals/src/main/java/br/course/elita/java/server/socket/SingleThreadSocketServer.java
@@ -10,11 +10,12 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.function.Consumer;
 
 @Slf4j
 public class SingleThreadSocketServer {
 
-    public static final int TOTAL_INTERATION_SOCKET = 3;
+    public static final int TOTAL_ITERATION_SOCKET = 3;
     public static final int SOCKET_PORT = 8000;
 
     private final ResolverHttpPath resolver;
@@ -27,24 +28,38 @@ public class SingleThreadSocketServer {
         try (ServerSocket serverSocket = new ServerSocket(SOCKET_PORT)) {
             log.info("Server started at " + SOCKET_PORT);
 
-            int count = TOTAL_INTERATION_SOCKET;
-            while (count > 0) {
-                try (Socket socket = serverSocket.accept()) {
+            repeat(TOTAL_ITERATION_SOCKET, serverSocket, (value) -> {
+                try (Socket socket = value.accept()) {
                     String body = read(socket);
 
                     SocketHttpHandler handler = resolver.resolve(body);
                     HandlerHttpResponse response = handler.execute();
 
                     write(socket, response);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
-                count--;
-            }
+            });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private void repeat(int amountTimes, ServerSocket socket, Consumer<ServerSocket> consumer) {
+        int count = amountTimes;
+
+        while (count > 0) {
+            log.info("-".repeat(50));
+            log.info("Call number: " + amountTimes);
+
+            consumer.accept(socket);
+            count--;
+        }
+    }
+
     private String read(Socket socket) throws IOException {
+        log.info("Reading...");
+
         InputStream inputStream = socket.getInputStream();
         StringBuilder sb = new StringBuilder();
 
@@ -54,16 +69,15 @@ public class SingleThreadSocketServer {
             sb.append((char) data);
         } while (inputStream.available() > 0);
 
-//        System.out.println("-".repeat(50));
-//        System.out.println("Recebeu:");
-//        System.out.println(sb);
         return sb.toString();
     }
 
     private static void write(Socket socket, HandlerHttpResponse response) throws IOException {
+        log.info("Writing...");
+
         PrintStream printStream = new PrintStream(socket.getOutputStream());
 
-        printStream.printf("HTTP/1.1 %d %s", response.status(), response.info());
+        printStream.printf("HTTP/1.1 %d %s\n", response.status(), response.info());
         printStream.println("Content-type: application/text; charset=UTF-8");
         printStream.println();
         printStream.println(response.body());
