7f7433d3b3da7e8780556c8f3d095dac3017992b
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-04-06 17:36:19 -0300

    pos-java-elite: react home

--- pos-java-elite/3-Frontend/fundamentals-react/src/app/page.tsx
+++ pos-java-elite/3-Frontend/fundamentals-react/src/app/page.tsx
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
-  return (
-    <div>Fundametals</div>
-  );
+  return (<>
+    <h1>Fundametals</h1>
+
+    <h3><Link href="/basic">Basic</Link></h3>
+    <h3><Link href="/intermediate">Intermediate</Link></h3>
+    <h3><Link href="/routes/victor">Intermediate: Routes - victor</Link></h3>
+    <h3><Link href="/routes/name-info/item-info">Intermediate: Routes - name-info and item</Link></h3>
+    <h3><Link href="/advance">Advance</Link></h3>
+  </>);
 }
