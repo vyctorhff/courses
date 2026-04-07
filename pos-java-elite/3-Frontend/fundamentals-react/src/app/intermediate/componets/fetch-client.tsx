693b3864f57a101fb795b83a37d98b7772d10ae4
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2026-04-07 13:25:51 -0300

    pos-java-elite: react fetch

--- pos-java-elite/3-Frontend/fundamentals-react/src/app/intermediate/componets/fetch-client.tsx
+++ pos-java-elite/3-Frontend/fundamentals-react/src/app/intermediate/componets/fetch-client.tsx
@@ -2,6 +2,17 @@
 
 import { FC, useEffect, useState } from "react";
 
+/**
+ * CORS
+ * 
+ * Proteção feita do servidor que o navegador respeita ao fazer uma requisição.
+ * 
+ * Fetch no client é usado quando:
+ *  - dado em tempo real
+ *  - depende da interação com o usuário
+ *  - requisição 'pesada': relatório
+ */
+
 type Post = {
     id: number,
     title: string,
@@ -18,13 +29,13 @@ export const FetchClient:FC = () => {
             fetch('https://jsonplaceholder.typicode.com/todos/1')
                 .then(response => response.json())
                 .then(json => setData({id: json.id, title: json.title}));
-        }, 5000);
+        }, 6000);
     });
     
-    if (loading) return (<>Loading fetch data....</>);
+    if (loading) return (<>Fetch Client: Loading fetch data....</>);
 
     return (<>
-        <h4>Resultado fetch</h4>
+        <h4>Fetch Client</h4>
         <span>id: {data?.id} - title: {data?.title}</span>
     </>);
 };
--- pos-java-elite/3-Frontend/fundamentals-react/src/app/intermediate/componets/fetch-server.tsx
+++ pos-java-elite/3-Frontend/fundamentals-react/src/app/intermediate/componets/fetch-server.tsx
@@ -1,1 +1,44 @@
-// todo!
+import { FC, useEffect, useState } from "react";
+
+/**
+ * Fetch no servidor
+ * 
+ * Quando usar:
+ *  - Interação com apis de outros sistemas
+ */
+
+type Post = {
+    id: number,
+    title: string,
+}
+
+const doRequest = async (): Promise<Post[]> => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+    let body: Post[] = await response.json();
+
+    return body.slice(0, 5);// diminuindo resultado
+};
+
+export const FetchServer:FC = () => {
+    const [loading, setLoading] = useState(true);
+    const [postList, setPostList] = useState<Post[]>([]);
+
+    useEffect(() => {
+        const request = async () => {
+            let body: Post[] = await doRequest();
+            setPostList(body);
+            setLoading(false);
+        };
+        
+        setTimeout(() => {
+            request();
+        }, 6000);
+    });
+
+    if (loading) return (<><span>Loading fetch server...</span></>);
+
+    return (<>
+        <h4>Fetch Server</h4>
+        {postList.map(post => <li key={post.id}>{post.title}</li>)}
+    </>);
+};
